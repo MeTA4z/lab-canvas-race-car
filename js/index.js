a1ee80da6b7fb52f20aad8705cd9571b26d48612
Rafael Rodriguez <rafaelarodriguez1126@gmail.com>
2021-03-25 18:06:31 -0400

    change btn clr

--- js/index.js
+++ js/index.js
@@ -5,50 +5,73 @@ let ctx = canvas.getContext('2d')
 // carImg.onload = animate
 let roadImg = new Image()
 roadImg.src = "./images/road.png"
-let road = {x: 0, y: 30, w: 450, h: 600, img: roadImg}
+// let road = {x: 0, y: 30, w: 450, h: 600, img: roadImg}
 
 // carImg.onload = animate
 let carImg = new Image()
-carImg.src = "./images/car.png"
-let car = { x: 0, y: 0, w: 100, h: 200, img: carImg}
+carImg.src = "./images/delor.png"
+// let car = { x: 0, y: 0, w: 100, h: 200, img: carImg}
 
 class Car {
-  constructor(x, y, w, h, img) {
+  constructor(img, x, y, w, h) {
+    this.img = img
     this.x = x
     this.y = y
     this.w = w
     this.h = h
-    this.img = img
   }
-  draw() {
-    ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
+draw() {
+  ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
   }
 }
 
-let toyota = new Car(0, 0, 100, 200, carImg)
+let delorean = new Car(carImg, 0, 0, 60, 120)
+
+function animate() {
+  let gameLoop = requestAnimationFrame(animate)
+  console.log("animate")
+  ctx.clearRect(0,0, canvas.width, canvas.height)
+  ctx.drawImage(roadImg, 0, 0, 450, 600)
+  // ctx.drawImage(toyota.img, toyota.x, toyota.y, toyota.w, toyota.h)
+  delorean.draw()
+  boxObstacle.draw()
+}
+animate()
+
 
 window.onkeydown = function(e) {
   console.log(e.key)
   switch (e.key) {
     case "ArrowLeft":
-      toyota.x -= 30;
-      // break;
+      delorean.x -= 30;
+      break;
       case "ArrowRight":
-        toyota.x += 30;
+      delorean.x += 30;
+      break;
+      case "ArrowUp":
+      delorean.y -= 30;
+      break;
+      case "ArrowDown":
+      delorean.y += 30;
+      break;
   }
 }
 
-
-function animate() {
-  let gameLoop = requestAnimationFrame(animate)
-  console.log("animate")
-  ctx.drawImage(roadImg, road.x, road.y, road.w, road.h)
-  ctx.drawImage(carImg, car.x, car.y, car.w, car.h)
+class Obstacle {
+  constructor(x, y, w, h, color) {
+    this.color = color
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+  }
+  draw() {
+    ctx.fillStyle = this.color
+    ctx.fillRect (this.x, this.y, this.w, this.h) 
+  }
 }
-animate()
 
-
-
+let boxObstacle = new Obstacle(50, 50, 50, 50, "light blue")
 
 
 
